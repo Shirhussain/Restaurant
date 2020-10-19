9accc022c701991801828a604d95f8836a7aebe3
حصین <sh.danishyar@gmail.com>
2020-10-20 00:33:08 +0330

    django tweaks added

--- conf/settings.py
+++ conf/settings.py
@@ -24,6 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third party
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- reservation/forms.py
+++ reservation/forms.py
@@ -3,6 +3,20 @@ from . models import Reservation
 
 
 class ReserveForm(forms.ModelForm):
+    # name = models.CharField(max_length=40)
+    # email = models.EmailField(max_length=60)
+    # phone = models.IntegerField()
+    # number_of_persons = models.IntegerField()
+    # date = models.DateField()
+    # time = models.TimeField()
+
+    # name = forms.CharField(widget=forms.TextInput(attrs={
+    #     "type": "email",
+    #     "name": "email",
+    #     "id": "email",
+    #     "placeholder": "Type your email address",
+    # }), label="")
+    
     class Meta:
         model = Reservation
         fields = "__all__"
